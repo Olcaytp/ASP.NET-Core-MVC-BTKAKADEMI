b8042860ce71f5cf46cb38ea5b74bc6a43a2bfee
olcay <olcaytapan10@gmail.com>
2023-09-16 11:01:20 +0200

    ch13.5-refactoring-sessionCart

--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -2,6 +2,7 @@ using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Services.Contracts;
+using StoreApp.Infrastructure.Extensions;
 
 namespace StoreApp.Pages
 {
@@ -12,16 +13,20 @@ namespace StoreApp.Pages
         public Cart Cart { get; set; } // IoC
         public string ReturnUrl { get; set; } = "/";
 
-        public CartModel(IServiceManager manager, Cart cart)
+        public CartModel(IServiceManager manager, Cart cartService)
         {
             _manager = manager;
-            Cart = cart;
+            Cart = cartService;
         }
 
 
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
+            //AddSingleton ile olusturulan Cart nesnesi, her request icin ayni nesneyi kullanir.
+            //AddScoped ile olusturulan Cart nesnesi, her request icin farkli bir nesne olusturur.
+            //AddScoped a gectikten sonra cartta surekli tek nesne olusturuluyordu, o hatanin onune gecmek icin
+            // Cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart(); //cartServiceten sonra bu ifadeye gerek yok
         }
 
         public IActionResult OnPost(int productId, string returnUrl)
@@ -32,14 +37,22 @@ namespace StoreApp.Pages
 
             if(product is not null)
             {
+                //ilgili ifadeyi sessiona kaydetmis olduk.
+                //sessioncart calistigi icin buna gerek yok artik
+                // Cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart(); //GetJson deserialize islemi yapar.
                 Cart.AddItem(product,1);
+                //sessionCart calistigi icin buna da gerek yok artik
+                // HttpContext.Session.SetJson("Cart",Cart);
             }
             return Page(); // returnUrl
         }
 
         public IActionResult OnPostRemove(int id, string returnUrl)
         {
+            //sessionCart calistigi icin buna da gerek yok artik
+            // Cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
             Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId.Equals(id)).Product);
+            // HttpContext.Session.SetJson("Cart",Cart); //sessionCart calistigi icin buna da gerek yok artik
             return Page(); 
         }
     }
--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -4,6 +4,7 @@ using Repositories;
 using Repositories.Contracts;
 using Services;
 using Services.Contracts;
+using StoreApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,9 @@ builder.Services.AddDbContext<RepositoryContext>(options =>
     //provides cache memory and session management
     builder.Services.AddDistributedMemoryCache();
     builder.Services.AddSession();
+    // builder.Services.AddHttpContextAccessor(); //ioc dahilinde kullanilmasi daha dogru olur
+    //httpContext ifadesine ersimek icin bir accessor'a ihtiyac var. bu da ioc ile saglanir.
+    builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 //IoC için interface ve concrete tanımlamaları yapılmalı.
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
@@ -28,8 +32,8 @@ builder.Services.AddScoped<IServiceManager, ServiceManager>();
 builder.Services.AddScoped<IProductService, ProductManager>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 
-builder.Services.AddSingleton<Cart>();  // One object for all requests, service registered as singleton
-
+// builder.Services.AddSingleton<Cart>();  // One object for all requests, service registered as singleton
+builder.Services.AddScoped<Cart>(c => SessionCart.GetCart(c));  // One object for each request, service registered as scoped
 builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
